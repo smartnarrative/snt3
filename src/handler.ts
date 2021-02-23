9df959ec8938715145b0b5927d959854f144bbb5
Woody Lewis <woody@woodylewis.com>
2021-02-23 11:08:26 -0500

    no message

--- src/handler.ts
+++ src/handler.ts
@@ -4,21 +4,26 @@ import getTokenCredentials from "./secret";
 import Web3 from "web3";
 const snftABI = require('./abi.json');
 
-export const snt1: APIGatewayProxyHandler = async (lambdaEvent, context) => {
+export const snft: APIGatewayProxyHandler = async (lambdaEvent, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   let statusCode = 200;
   let message;
   try {
-    const t = await getTokenCredentials();
-    const tObj = JSON.parse(t);
-    const testnet = `${process.env.INFURA_URL}${tObj.PROVIDER_ACCESS_TOKEN}`;
-    const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
-    web3.eth.defaultAccount = tObj.OWNER_ADDRESS;
-    const wallet = web3.eth.defaultAccount;
-    const theContract = new web3.eth.Contract(snftABI, tObj.NFT_CONTRACT_ADDRESS);
-    const metaDataHash = await theContract.methods.tokenURI('001').call({from: wallet});
-    message = metaDataHash;
+    const postParams = JSON.parse(lambdaEvent.body);
+    if (!postParams.tokenId) {
+      statusCode = 500;
+      message = 'No Token Id';
+    } else {
+      const t = await getTokenCredentials();
+      const tObj = JSON.parse(t);
+      const testnet = `${process.env.INFURA_URL}${tObj.PROVIDER_ACCESS_TOKEN}`;
+      const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+      web3.eth.defaultAccount = tObj.OWNER_ADDRESS;
+      const wallet = web3.eth.defaultAccount;
+      const theContract = new web3.eth.Contract(snftABI, tObj.NFT_CONTRACT_ADDRESS);
+      message = await theContract.methods.tokenURI(postParams.tokenId).call({from: wallet});
+    }
   } catch (error) {
     // eslint-disable-next-line
     console.log(`error ${JSON.stringify(error)}`);
