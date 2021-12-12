ec267761054ddd7722bfae2f0da3f654fb9b0280
Woody Lewis <woody@woodylewis.com>
2021-12-12 16:55:53 -0500

    update solidity

--- src/snft.sol
+++ src/snft.sol
@@ -1,52 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.6;
 
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-enumerable.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
-/**
- * @dev This is an example contract implementation of NFToken with metadata extension.
- */
-contract SNFT is
-NFTokenMetadata,
-Ownable
-{
+contract snnNFT is NFTokenMetadata, Ownable {
 
-    /**
-     * @dev Contract constructor. Sets metadata extension `name` and `symbol`.
-     */
-    constructor()
-    {
-        nftName = "Smart Narrative";
-        nftSymbol = "SNFT";
+    constructor() {
+        nftName = "SmartNarrative";
+        nftSymbol = "SNN";
     }
 
-    /**
-     * @dev Mints a new NFT.
-     * @param _to The address that will own the minted NFT.
-     * @param _tokenId of the NFT to be minted by the msg.sender.
-     * @param _uri String representing RFC 3986 URI.
-     */
-    function createNode(
-        address _to,
-        uint256 _tokenId,
-        string calldata _uri
-    )
-    external
-    onlyOwner
-    {
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
         super._mint(_to, _tokenId);
         super._setTokenUri(_tokenId, _uri);
     }
 
-    function deleteNode(
-        uint256 _tokenId
-    )
-    external
-    onlyOwner
-    {
-        super._burn(_tokenId);
+    function getTokenURI(uint256 _tokenId) external view onlyOwner returns (string memory) {
+        return super._tokenURI(_tokenId);
     }
-
 }
