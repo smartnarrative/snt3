dc4b77612a34ceca4775c55f1b44f14d9c801d6a
Woody Lewis <woody@woodylewis.com>
2021-12-14 21:32:04 -0500

    no message

--- src/handler.ts
+++ src/handler.ts
@@ -12,28 +12,18 @@ export const snft: APIGatewayProxyHandler = async (lambdaEvent, context) => {
   let message;
   try {
     const postParams = JSON.parse(lambdaEvent.body);
-    console.log('postParams');
-    console.dir(postParams);
     if (!postParams.tokenId) {
       statusCode = 500;
       message = 'No Token Id';
     } else {
       const t = await getTokenCredentials();
       const tObj = JSON.parse(t);
-      console.log('getTokenCredentials ==> ');
-      console.dir(tObj);
       const testnet = `${process.env.INFURA_URL}${tObj.PROVIDER_ACCESS_TOKEN}`;
-      console.log(`testnet ==> ${testnet}`);
       const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
-      // console.log('web3 ==>');
-      // console.dir(web3);
       web3.eth.defaultAccount = tObj.OWNER_ADDRESS;
       const wallet = web3.eth.defaultAccount;
       const theContract = new web3.eth.Contract(snftABI, tObj.NFT_CONTRACT_ADDRESS);
-      // console.log('theContract ==>');
-      // console.dir(theContract);
       const ipfsHash = await theContract.methods.getTokenURI(postParams.tokenId).call({from: wallet});
-      console.log(`ipfsHash => ${ipfsHash}`);
       const ipfsResponse = await axios({
         method: 'get',
         url: ipfsHash,
