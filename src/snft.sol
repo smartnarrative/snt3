a85658c56431fcbfa8d51807773ba1d447824857
Woody Lewis <woody@woodylewis.com>
2021-12-15 20:31:00 -0500

    no message

--- src/snft.sol
+++ src/snft.sol
@@ -4,19 +4,31 @@ pragma solidity 0.8.6;
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
-contract snnNFT is NFTokenMetadata, Ownable {
+contract SnnNFT is NFTokenMetadata, Ownable {
 
     constructor() {
         nftName = "SmartNarrative";
         nftSymbol = "SNN";
     }
 
+    event SnnEvent (
+        uint256 id,
+        string message
+    );
+
     function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
         super._mint(_to, _tokenId);
         super._setTokenUri(_tokenId, _uri);
+
+        emit SnnEvent(_tokenId, "MINTED");
+    }
+
+    function burn(uint256 _tokenId) external onlyOwner {
+        super._burn(_tokenId);
     }
 
     function getTokenURI(uint256 _tokenId) external view onlyOwner returns (string memory) {
         return super._tokenURI(_tokenId);
     }
+
 }
